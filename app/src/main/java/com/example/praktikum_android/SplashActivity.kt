43c6bec32eac2e975ec01b7633644abf08bc249a
Vicco Dwi <50991950+vicco7s@users.noreply.github.com>
2021-11-14 21:40:02 +0800

    Loading Progress

--- app/src/main/java/com/example/praktikum_android/SplashActivity.kt
+++ app/src/main/java/com/example/praktikum_android/SplashActivity.kt
@@ -17,7 +17,7 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },2000)
+        },5000)
 
         
     }
